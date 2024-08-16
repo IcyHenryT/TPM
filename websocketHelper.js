056e2e0c929f036f11da478ae284bd0a7af2d17e
IcyHenryT <icyhenryt@gmail.com>
2024-08-16 14:55:50 -0400

    Version bump
    Forgot to change version

--- websocketHelper.js
+++ websocketHelper.js
@@ -32,7 +32,7 @@ async function startWS(sid) {
         logmc('§aConnected to WebSocket server');
         if (webhook) {
             const embed = new MessageBuilder()
-                .setFooter(`The "Perfect" Macro - v1.1.7`, 'https://media.discordapp.net/attachments/1223361756383154347/1263302280623427604/capybara-square-1.png?ex=6699bd6e&is=66986bee&hm=d18d0749db4fc3199c20ff973c25ac7fd3ecf5263b972cc0bafea38788cef9f3&=&format=webp&quality=lossless&width=437&height=437')
+                .setFooter(`The "Perfect" Macro - v1.1.8`, 'https://media.discordapp.net/attachments/1223361756383154347/1263302280623427604/capybara-square-1.png?ex=6699bd6e&is=66986bee&hm=d18d0749db4fc3199c20ff973c25ac7fd3ecf5263b972cc0bafea38788cef9f3&=&format=webp&quality=lossless&width=437&height=437')
                 .setTitle('Started flipping')
                 .addField('', `Logged in as \`${config.username}\``)
                 .setThumbnail(`https://mc-heads.net/head/${config.uuid}.png`)
